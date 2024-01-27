c5b1aaf1639e7adc30ef205375b1b0dff6835ca0
Tdejong765 <tdejong765@gmail.com>
2024-01-27 15:31:34 +0100

    Refactor to use class Hive.php

---
+++ src/OLD/database.php
@@ -0,0 +1,14 @@
+<?php
+
+function getState() {
+    return serialize([$_SESSION['hand'], $_SESSION['board'], $_SESSION['player']]);
+}
+
+function setState($state) {
+    list($a, $b, $c) = unserialize($state);
+    $_SESSION['hand'] = $a;
+    $_SESSION['board'] = $b;
+    $_SESSION['player'] = $c;
+}
+
+return new mysqli('mysql-db', 'username', 'password', 'hive', 3306);
--- src/OLD/index.php
+++ src/OLD/index.php
@@ -1,15 +1,28 @@
 <?php
     session_start();
 
-    include_once 'Hive.php';
+    include_once 'util.php';
 
     if (!isset($_SESSION['board'])) {
-        header('Location: Index.php');
+        header('Location: restart.php');
         exit(0);
     }
-   
-    $Hive = new Hive($_SESSION['board'], $_SESSION['player'], $_SESSION['hand']);
+    $board = $_SESSION['board'];
+    $player = $_SESSION['player'];
+    $hand = $_SESSION['hand'];
 
+    $to = [];
+    foreach ($GLOBALS['OFFSETS'] as $pq) {
+        foreach (array_keys($board) as $pos) {
+            $pq2 = explode(',', $pos);
+            $to[] = ($pq[0] + $pq2[0]).','.($pq[1] + $pq2[1]);
+        }
+    }
+    
+    $to = array_unique($to);
+    if (!count($to)){
+         $to[] = '0,0';
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
