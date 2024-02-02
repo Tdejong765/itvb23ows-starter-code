e729dc2d722fa51102b6df6fba83a6227df8b510
Tdejong765 <tdejong765@gmail.com>
2024-02-02 15:10:30 +0100

    Added test for: "De dropdown die aangeeft welke stenen een speler kan plaatsen bevat ook stenen die de speler niet meer heeft"

---
+++ Tests/hiveViewTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class hiveViewTest extends TestCase {
+
+    public function testShowTiles() {
+        $gameControllerMock = $this->getMockBuilder(GameController::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $gameControllerMock->method('getHand')
+            ->willReturn([
+                'player1' => ['stone1' => 2, 'stone2' => 1],
+                'player2' => ['stone3' => 3, 'stone4' => 1],
+            ]);
+
+        $hiveView = new hiveView($gameControllerMock);
+
+        $options = $hiveView->showTiles('player1');
+
+        $expectedOptions = [
+            '<option value="stone1">stone1</option>',
+            '<option value="stone2">stone2</option>',
+        ];
+
+        $this->assertEquals($expectedOptions, $options);
+    }        
+}
