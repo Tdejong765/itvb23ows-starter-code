64e9d6e282b7c0d1eaaef05702164e6ea6f591cf
Tdejong765 <tdejong765@gmail.com>
2024-03-29 14:19:44 +0100

    Added boardControllerTest skeleton

---
+++ Tests/boardControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../src/MVC/Controller/gameController.php';
+require_once __DIR__ . '/../src/MVC/Controller/boardController.php';
+require_once __DIR__ . '/../src/MVC/Controller/sessionController.php';
+require_once __DIR__ . '/../src/MVC/View/hiveView.php';
+require_once __DIR__ . '/../src/MVC/Model/hiveModel.php';
+
+
+class boardControllerTest extends TestCase {}
